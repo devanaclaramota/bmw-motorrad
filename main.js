71a2a894404c4b369905096114b4a74d66d8c81f
Ana clara <dev.anaclaramota@outlook.com>
2023-01-12 01:04:19 -0300

    estrutura

---
+++ main.js
@@ -0,0 +1,18 @@
+
+     let btn = document.querySelector('.openNav')
+     btn.addEventListener('click',menu);
+     function menu(){
+     let nav =  document.querySelector('.menu')
+     nav.classList.toggle('ativar')
+     }
+  
+     function openTab(idtab){
+          let content = document.querySelectorAll('.tab-menu-album');
+          for(i=0; i < content.length;i++){
+               content[i].style.display = 'none'
+          }
+          document.getElementById(idtab).style.display = 'block';
+
+
+     }
+   
